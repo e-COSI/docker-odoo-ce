163feb3d13ca42f4f30628392e7817627c3fb1c5
Lionel Déglise <lionel.deglise@e-cosi.com>
2019-02-19 16:24:34 +0100

    [FIX] sed withour inline editing

--- files/patch_release.sh
+++ files/patch_release.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#cp /odoo/odoo-server/odoo/release.py /odoo/odoo-server/odoo/release_py.backup
-set +x
-cat /odoo/odoo-server/odoo/release.py
-sed -i.orig "s/(version_info = \([0-9]+, [0-9]+, [0-9]+, FINAL, [0-9]+, ')()('\))/\1${1//-/}-${2:0:7}\3/g" /odoo/odoo-server/odoo/release.py
+# Can't do inline editing work :-(
+cp /odoo/odoo-server/odoo/release.py /odoo/odoo-server/odoo/release.py.orig
+cat /odoo/odoo-server/odoo/release.py.orig | sed -r "s/(version_info = \([0-9]+, [0-9]+, [0-9]+, FINAL, [0-9]+, ')()('\))/\1${1//-/}-${2:0:7}\3/g" /odoo/odoo-server/odoo/release.py > /odoo/odoo-server/odoo/release.py
 #sed -i "s/%%CONTAINER_DATE%%/${1//-/}/g" /tmp/release.diff
 #sed -i "s/%%COMMIT_HASH%%/${2:0:7}/g" /tmp/release.diff
 #patch -i /tmp/release.diff /odoo/odoo-server/odoo/release.py
