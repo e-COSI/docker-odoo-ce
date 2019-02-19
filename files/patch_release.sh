9388da24b6149e54f9d19bfde0efc0283cd09adb
Lionel Déglise <lionel.deglise@e-cosi.com>
2019-02-19 16:08:24 +0100

    [FIX] regex error

--- files/patch_release.sh
+++ files/patch_release.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #cp /odoo/odoo-server/odoo/release.py /odoo/odoo-server/odoo/release_py.backup
-sed -i.orig "s/(version_info = \([0-9]+, [0-9]+, [0-9]+, FINAL,[0-9]+, ')()('\))/\1${1//-/}-${2:0:7}\3/g" /odoo/odoo-server/odoo/release.py
+set +x
+cat /odoo/odoo-server/odoo/release.py
+sed -i.orig "s/(version_info = \([0-9]+, [0-9]+, [0-9]+, FINAL, [0-9]+, ')()('\))/\1${1//-/}-${2:0:7}\3/g" /odoo/odoo-server/odoo/release.py
 #sed -i "s/%%CONTAINER_DATE%%/${1//-/}/g" /tmp/release.diff
 #sed -i "s/%%COMMIT_HASH%%/${2:0:7}/g" /tmp/release.diff
 #patch -i /tmp/release.diff /odoo/odoo-server/odoo/release.py
